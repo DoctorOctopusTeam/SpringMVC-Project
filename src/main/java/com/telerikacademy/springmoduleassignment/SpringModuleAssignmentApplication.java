58c7652abdff0ca520cd9d734ded760a10879b1b
Deyan Peychev <dido_peichev@yahoo.com>
2018-07-23 18:07:12 +0300

    Add project structure

---
+++ src/main/java/com/telerikacademy/springmoduleassignment/SpringModuleAssignmentApplication.java
@@ -0,0 +1,12 @@
+package com.telerikacademy.springmoduleassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringModuleAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringModuleAssignmentApplication.class, args);
+    }
+}
---
+++ src/test/java/com/telerikacademy/springmoduleassignment/SpringModuleAssignmentApplicationTests.java
@@ -0,0 +1,16 @@
+package com.telerikacademy.springmoduleassignment;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringModuleAssignmentApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
