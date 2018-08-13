61340ee3c9f98cb680c8c1120da03067c3f698d4
Atanas Velev <velevatanas@yahoo.com>
2018-08-13 17:10:01 +0300

    Furter improvment

--- src/main/java/com/hibernate/hibernatedemo/controllers/HelloController.java
+++ src/main/java/com/hibernate/hibernatedemo/controllers/HelloController.java
@@ -1,35 +1,55 @@
 package com.hibernate.hibernatedemo.controllers;
 
 import com.hibernate.hibernatedemo.models.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HelloController {
 
     @RequestMapping("/first")
     public String hi(Model model, HttpSession httpSession){
-        User u = new User("Pesho");
-        model.addAttribute("pederaz", u);
-        //model.addAttribute("name","Motherfucker");
+        User u = new User("Pesho", 5);
+        //model.addAttribute("pederaz", u);
+        model.addAttribute("name1","Motherfucker");
         httpSession.setAttribute("name", "Gosho");
-        return "firstView";
+        return "firstView" ;//:: #nasko";
     }
 
     @GetMapping("/second")
     public ModelAndView mav(){
-        User u = new User("Pesho");
+        User u = new User("Pesho",7);
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("");
+        modelAndView.setViewName("second");
         modelAndView.addObject("pederaz", u);
         return modelAndView;
+    }
+
+    @GetMapping("")
+    public String vuk(){
+        return "index";
+    }
+
+    @GetMapping("/loop")
+    public String cuk(Model model){
+        User user1 = new User("Zasko", 10);
+        User user2 = new User("Vasko", 11);
+        User user3 = new User("Jasko", 12);
+        List<User> list = new ArrayList<>();
+        list.add(user1);
+        list.add(user2);
+        list.add(user3);
+        for (int i = 1; i <100; i++){
+            list.add(new User(Integer.toString(i), i + 20));
+        }
+        model.addAttribute("list", list);
+        //return "second :: frag";
+        return "loop";
     }
 }
--- src/main/java/com/hibernate/hibernatedemo/models/User.java
+++ src/main/java/com/hibernate/hibernatedemo/models/User.java
@@ -1,13 +1,20 @@
 package com.hibernate.hibernatedemo.models;
 
 public class User {
+
+
+    private int id;
     private String name;
 
-    public User(String name){
+    public User(String name, int id){
         this.name = name;
+        this.id = id;
     }
 
     public String getName() {
         return name;
+    }
+    public int getId() {
+        return id;
     }
 }
